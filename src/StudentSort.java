34a027c4ab07a5ad3a6c259e59e68c6bcd2d335f
cherryannbunga <cherry-ann.bunga@bisu.edu.ph>
2025-04-11 21:52:53 +0800

    Add files via upload

---
+++ src/StudentSort.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class StudentSort {
+    String name;
+    double grade;
+
+    StudentSort(String name, double grade){
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many Students?");
+        int numStudents = scanner.nextInt();
+        scanner.nextLine();
+        List<StudentSort> students = new ArrayList<>();
+
+        for (int i=0; i<numStudents; i++){
+            System.out.println("Enter Details for student#" + (i+1));
+            System.out.println("Name: ");
+            String name = scanner.nextLine();
+            System.out.println("Grade");
+            double grade = scanner.nextDouble();
+            scanner.nextLine();
+
+            students.add(new StudentSort(name,grade));
+        }
+
+        for (int i=0; i< students.size() - 1; i++){
+            for(int j=0; j< students.size() - i -1; j++){
+                if(students.get(j).grade > students.get(j + 1).grade){
+
+                    StudentSort temp = students.get(j);
+                    students.set(j, students.get(j + 1));
+                    students.set(j + 1, temp);
+                }
+            }
+        }
+
+        System.out.println("\nSorted Students by Grade  (Ascending):");
+        for(StudentSort student : students ){
+            System.out.println(student.name + " - " + student.grade);
+        }
+
+        scanner.close();
+    }
+}
